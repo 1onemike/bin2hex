c8d783739492f6d74f23b5bb6c436baeadf4625a
Michael Osei <moaduamah@gmail.com>
2020-05-25 07:39:12 -0400

    initial files

---
+++ BIN2HEXNEW/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace BIN2HEXNEW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TextBox2 = new System.Windows.Forms.TextBox();
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.Button3 = new System.Windows.Forms.Button();
+            this.Button2 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Button4
+            // 
+            this.Button4.Location = new System.Drawing.Point(222, 224);
+            this.Button4.Name = "Button4";
+            this.Button4.Size = new System.Drawing.Size(216, 27);
+            this.Button4.TabIndex = 17;
+            this.Button4.Text = "Save Hex";
+            this.Button4.UseVisualStyleBackColor = true;
+            this.Button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(164, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(196, 21);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Bin To Hex Converter";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Result :";
+            // 
+            // TextBox2
+            // 
+            this.TextBox2.Location = new System.Drawing.Point(95, 83);
+            this.TextBox2.Multiline = true;
+            this.TextBox2.Name = "TextBox2";
+            this.TextBox2.Size = new System.Drawing.Size(369, 123);
+            this.TextBox2.TabIndex = 14;
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Location = new System.Drawing.Point(107, 228);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(100, 20);
+            this.TextBox1.TabIndex = 13;
+            // 
+            // Button3
+            // 
+            this.Button3.Location = new System.Drawing.Point(338, 254);
+            this.Button3.Name = "Button3";
+            this.Button3.Size = new System.Drawing.Size(100, 27);
+            this.Button3.TabIndex = 12;
+            this.Button3.Text = "About";
+            this.Button3.UseVisualStyleBackColor = true;
+            this.Button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Button2
+            // 
+            this.Button2.Location = new System.Drawing.Point(222, 254);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(100, 27);
+            this.Button2.TabIndex = 11;
+            this.Button2.Text = "Convert";
+            this.Button2.UseVisualStyleBackColor = true;
+            this.Button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(107, 254);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 27);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Open .Bin";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MV Boli", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(170, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(188, 39);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Small Tech ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(520, 291);
+            this.Controls.Add(this.Button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TextBox2);
+            this.Controls.Add(this.TextBox1);
+            this.Controls.Add(this.Button3);
+            this.Controls.Add(this.Button2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label5);
+            this.HelpButton = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "BIN2HEX";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Button Button4;
+        internal System.Windows.Forms.Label label2;
+        internal System.Windows.Forms.Label label4;
+        internal System.Windows.Forms.TextBox TextBox2;
+        internal System.Windows.Forms.TextBox TextBox1;
+        internal System.Windows.Forms.Button Button3;
+        internal System.Windows.Forms.Button Button2;
+        internal System.Windows.Forms.Button button5;
+        internal System.Windows.Forms.Label label5;
+
+    }
+}
+
---
+++ BIN2HEXNEW/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BIN2HEXNEW
+{
+    public partial class Form1 : Form
+    {
+        String abc;
+        String filename;
+     
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.TextBox2.Multiline = true;
+            this.TextBox2.WordWrap = true;
+            this.TextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+           
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+          
+        }
+
+
+   
+        
+     
+      
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog theDialog = new OpenFileDialog();
+            theDialog.Title = "Open Bin File";
+            theDialog.Filter = "BIN files|*.bin";
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filename = theDialog.FileName;
+                TextBox1.Text = filename;
+
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(TextBox1.Text)) 
+            {
+                String path = TextBox1.Text;
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                startInfo.FileName = "bin_hex.cmd";
+                startInfo.Arguments = "\"" + path + "\"";
+                process.StartInfo = startInfo;
+                process.Start();
+                filename = path.Substring(0, path.LastIndexOf('.'));
+                abc = filename + ".hex";
+                if (File.Exists(abc))
+                {
+                    using (StreamReader sr = new StreamReader(abc))
+                    {
+                        String line;
+
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            TextBox2.AppendText(line + "\n");
+                        }
+                    }
+                    File.Delete(abc);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select BIN file!");
+            }
+           
+
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Small Tech Pixel Software Soulution");
+
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(TextBox2.Text))
+            {
+                SaveFileDialog savefile = new SaveFileDialog();
+                // set a default file name
+                savefile.FileName = Path.GetFileName(abc);
+                // set filters - this can be done in properties as well
+                savefile.Filter = "Hex files (*.hex)|*.hex";
+
+                if (savefile.ShowDialog() == DialogResult.OK)
+                {
+                    using (StreamWriter sw = new StreamWriter(savefile.FileName))
+                        sw.WriteLine(TextBox2.Text);
+                }
+                MessageBox.Show("Hex Saved Successfully");
+            }
+            else 
+            {
+                MessageBox.Show("HEX not generated!");
+
+            }
+          
+
+
+        }
+
+
+    }
+}
